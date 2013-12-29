2323cf3b6280cf254fffa5878211aa14602df748
Ubuntu <ubuntu@ip-10-185-10-143.ec2.internal>
2013-12-29 07:36:00 +0000

    last 3 steps pending on merge feature

--- features/models/step_definitions/article_steps.rb
+++ features/models/step_definitions/article_steps.rb
@@ -10,23 +10,35 @@ When /I should not see the merge articles feature/ do
 end
 
 When /^I start editing an article$/ do
-  visit '/admin/content/new'
-  fill_in("article_title", :with => "Foobar") 
-  fill_in("article__body_and_extended_editor", :with => "Lorem Ipsum")
-  click_button("Publish")
-  current_path = URI.parse(current_url).path
-  current_path.should == "/admin/content"
-  page.should have_xpath('//*', :text => "Foobar")
+  create_article "Foobar", "Lorem Ipsum"
   click_link("Foobar")
   page.should have_xpath('//*', :text => "Lorem Ipsum")
 end
 
 Given /^there is an article titled "(.*?)" with text of "(.*?)"$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+  create_article arg1, arg2
 end
 
 When /^I merge the article titled "(.*?)" with the article titled "(.*?)"$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+  debugger
+
+  id_merge_into = Article.where(:title => arg1).first.id
+  id_merge_with = Article.where(:title => arg2).first.id
+
+  visit '/admin/content'
+  current_path = URI.parse(current_url).path
+  page.should have_xpath('//*', :text => arg1)
+
+  click_link arg1
+  current_path = URI.parse(current_url).path
+  
+  fill_in 'merge_with', :with => id_merge_with
+  
+  click_button 'Merge'
+  
+  current_path = URI.parse(current_url).path
+  current_path.should == "/admin/content/edit/"+id_merge_into.to_s
+
 end
 
 Then /^the article titled "(.*?)" must still exist$/ do |arg1|
@@ -39,5 +51,16 @@ end
 
 Then /^the article titled "(.*?)" must have text of "(.*?)"$/ do |arg1, arg2|
   pending # express the regexp above with the code you wish you had
+end
+
+
+def create_article(title, text)
+  visit '/admin/content/new'
+  fill_in("article_title", :with => title) 
+  fill_in("article__body_and_extended_editor", :with => text)
+  click_button("Publish")
+  current_path = URI.parse(current_url).path
+  current_path.should == "/admin/content"
+  page.should have_xpath('//*', :text => title)
 end
 
